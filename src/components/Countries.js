33531b19ca8117b764739ebc98bde5e784b5e8d8
cedric.anaky.ext <heinzcodeur@gmail.com>
2024-02-09 12:26:01 +0100

    first

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -2,344 +2,215 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Navigation from "./Navigation";
 
-
 const Countries = () => {
-    const apikeys = [
-      "7a3522b864msh458ad29cbfda53dp191cd6jsn8a9ecb8f7113",
-      "50736dc75amsh7f9e578a03cb22ap1ea0e4jsn338832cda913",
-      "102585ac4fmsh2631463ec6a8969p1ccd25jsn4b73c4307617"
-    ];
-    const tennis_live = "https://sports-live-scores.p.rapidapi.com/tennis/live";
-    const tennis_bis = "https://allsportsapi2.p.rapidapi.com/api/tennis/events/18/7/2023";
-  
-    const [donnees, setData] = useState([]);
-    const [limitRequestLeft, setLimitRequestLeft] = useState(0);
-    const [intervalId, setIntervalId] = useState(1000);
-    const [apiKey, setApiKey] = useState(apikeys[0]);
-    const [danger, setDanger] = useState(null);
-    const [delay, setDelay] = useState(1500000);
-  
-    const fetchData = () => {
-      axios
-        .get(tennis_live, {
-          headers: { "X-RapidAPI-Key": apiKey },
-        })
-        .then((res) => {
-          console.log(limitRequestLeft);
-          setData(res.data.matches);
-          console.log(donnees);
-          setLimitRequestLeft(res.headers['x-ratelimit-requests-remaining']);
-        })
-        .catch((error) => {
-          console.log(error.config.headers['X-RapidAPI-Key']);
-          if (error.response.headers['x-ratelimit-requests-remaining'] <= 0) {
-            const currentIndex = apikeys.indexOf(apiKey);
-            const nextIndex = (currentIndex + 1) % apikeys.length;
-            const newApiKey = apikeys[nextIndex];
-            console.log(newApiKey);
-            setApiKey(newApiKey);
-            setDanger(error.response.data.message + '<br> code HTTP: ' + error.request.status);
-          }
-        });
-    };
-  
-    useEffect(() => {
-      fetchData();
-  
-      const intervalId = setInterval(fetchData, delay);
-  
-      return () => clearInterval(intervalId);
-    }, [apiKey, delay]);
+  const apikeys = [
+    "61cb8bddf7msh35df4200e00b592p143755jsnf183cbe708b5",
+    "be38a0a2c6msh898d50a07d5c391p1e7213jsne241f1052e89",
+    "aa3e2fc2f5msh1fb9e7704ed333cp1c64e0jsn411370dcfebf",
+    "7a3522b864msh458ad29cbfda53dp191cd6jsn8a9ecb8f7113",
+    "50736dc75amsh7f9e578a03cb22ap1ea0e4jsn338832cda913",
+    "102585ac4fmsh2631463ec6a8969p1ccd25jsn4b73c4307617",
+  ];
+  const tennis_live = "https://sports-live-scores.p.rapidapi.com/tennis/live";
+  //const tennis_bis = "https://allsportsapi2.p.rapidapi.com/api/tennis/events/18/7/2023";
 
-    return (
-        <div>
-          <Navigation />
-          <div className="container">
-            <div className="row">
-              {danger ? (
-                <div className="col-10 alert alert-danger mx-auto">{danger}</div>
-              ) : null}
-            </div>
+  const rank_wta =
+    "https://sports-live-scores.p.rapidapi.com/tennis/rankings/wta/500";
+
+  const checkBackSlash = (inputString) => {
+    const regex = /\//; // Expression régulière pour rechercher le symbole "/"
+    return regex.test(inputString); // Vérifie si la chaîne contient "/"
+  };
+
+  const [donnees, setData] = useState([]);
+  const [wtaRanking, setWtaRanking] = useState([]);
+  const [limitRequestLeft, setLimitRequestLeft] = useState(0);
+  const [limitRankWtaLeft, setLimitRankWtaLeft] = useState(0);
+  const [intervalId, setIntervalId] = useState(1000);
+  const [apiKeyIndex, setApiKeyIndex] = useState(0); // Ajout de l'index de la clé API
+  const [danger, setDanger] = useState(null);
+  const [delay, setDelay] = useState(15000);
+
+  const currentApiKey = apikeys[0];
+
+  const fetchWtaRanking = () => {
+    axios
+      .get(rank_wta, {
+        headers: {
+          "X-RapidAPI-Key": currentApiKey,
+        },
+      })
+      .then((res) => {
+        console.log(limitRequestLeft);
+        setWtaRanking(res.data.rankings);
+        console.log(wtaRanking[2]);
+        setLimitRankWtaLeft(res.headers["x-ratelimit-requests-remaining"]);
+        console.log(limitRankWtaLeft);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  };
+
+  const fetchData = () => {
+    axios
+      .get(tennis_live, {
+        headers: { "X-RapidAPI-Key": currentApiKey },
+      })
+      .then((res) => {
+        setDanger("");
+        setData(res.data.matches);
+        console.log(donnees.length);
+        setLimitRequestLeft(res.headers["x-ratelimit-requests-remaining"]);
+        console.log(limitRequestLeft);
+      })
+      .catch((error) => {
+        if (error.response.status === 429) {
+          const nextIndex = (apiKeyIndex + 1) % apikeys.length;
+          setApiKeyIndex(nextIndex);
+          // alert(apiKeyIndex)
+          const newApiKey = apikeys[nextIndex];
+          //alert(newApiKey);
+          setDanger(
+            error.response.data.message +
+              "<br> code HTTP: " +
+              error.request.status
+          );
+        }
+      });
+  };
+
+  useEffect(() => {
+    if (wtaRanking.length === 0) {
+    fetchWtaRanking();
+    }
+
+    fetchData();
+    const intervalId = setInterval(fetchData, delay);
+
+    return () => clearInterval(intervalId);
+  }, [apiKeyIndex, delay, currentApiKey]);
+
+  return (
+    <div>
+    <Navigation />
+    <div className="container border border-primary mx-auto">
+      <div className="row">
+        <div className="col-10 mx-auto">
+          <div>
+            {danger ? (
+              <div className="col-10 alert alert-danger mx-auto">
+                {danger}
+              </div>
+            ) : null}
           </div>
-          <hr />
-          <h3 className="text-center">
+          <h3 className="text-center text-success">
             Il te reste {limitRequestLeft} appels{" "}
+          </h3>
+          <p className="text-center">
             <button
               className="btn btn-primary"
               onClick={() => {
-                clearInterval(intervalId); // D'abord, effacez l'intervalle actuel
-                const newIntervalId = setInterval(fetchData, 20000); // Ensuite, créez un nouvel intervalle avec le délai souhaité
-                setIntervalId(newIntervalId); // Mettez à jour l'état intervalId avec le nouvel identifiant d'intervalle
+                setDelay(300000);
+                console.log(delay);
               }}
             >
               time
             </button>
-          </h3>
-          <table className="table table-striped">
-            <thead>
-              <tr>
-                <th>&nbsp;&nbsp;&nbsp;</th>
-                <th className="text-center">liste des matchs de ce jour</th>
-                <th>&nbsp;&nbsp;&nbsp;</th>
-              </tr>
-            </thead>
-            <tbody>
-              {donnees.map((match, index) => (
-                <tr key={index} className="border-bottom">
-                  <td>
-                    {match["Tournament"]} - {match["Surface"]}
-                  </td>
-                  <td>
-                    <tr>
-                      <td>{match["Home Player"]}</td>
+          </p>
+          <div className="overflow-auto">
+            <table className="table table-striped">
+              <thead>
+                {/* <tr>
+                  <th></th>
+                  <th className="text-center">liste des matchs de ce jour</th>
+                  <th>&nbsp;&nbsp;&nbsp;</th>
+                </tr> */}
+              </thead>
+  
+              <tbody>
+                {donnees.map((match, index) =>
+                  !checkBackSlash(match["Home Player"]) ? (
+                    <tr key={index} className="border-bottom">
+                      <td className="large-width-td">
+                        {match["Tournament"]} - {match["Surface"]}
+                      </td>
                       <td>
-                        <b>{match["Live Home Odd"]}</b>
+                        <tr>
+                          <td>{match["Home Player"]}</td>
+                          <td>
+                            <b>{match["Live Home Odd"]}</b>
+                          </td>
+                          {/* <td><b>{match["Sets Player 1"]}</b></td> */}
+                        </tr>
+                        <tr>
+                          <td>{match["Away Player"]} &nbsp;</td>
+                          <td>
+                            <b>{match["Live Away Odd"]}</b>
+                          </td>
+                          {/* <td><b>{match["Sets Player 2"]}</b></td> */}
+                        </tr>
                       </td>
-                      {/* <td><b>{match["Sets Player 1"]}</b></td> */}
-                    </tr>
-                    <tr>
-                      <td>{match["Away Player"]} &nbsp;</td>
                       <td>
-                        <b>{match["Live Away Odd"]}</b>
+                        <tr>
+                          <td className="text-success">
+                            {match["Sets Player 1"]}
+                          </td>
+                        </tr>
+                        <tr>
+                          <td className="text-success">
+                            {match["Sets Player 2"]}
+                          </td>
+                        </tr>
                       </td>
-                      {/* <td><b>{match["Sets Player 2"]}</b></td> */}
-                    </tr>
-                  </td>
-                  <td>
-                    <tr>
-                      <td>{match["Sets Player 1"]}</td>
-                    </tr>
-                    <tr>
-                      <td>{match["Sets Player 2"]}</td>
-                    </tr>
-                  </td>
-                  <td>
-                    <tr>
-                      <td>{match["Set1 Player 1"]}</td>
-                    </tr>
-                    <tr>
-                      <td>{match["Set1 Player 2"]}</td>
-                    </tr>
-                  </td>
-                  <td>
-                    <tr>
-                      <td>{match["Set2 Player 1"]}</td>
-                    </tr>
-                    <tr>
-                      <td>{match["Set2 Player 2"]}</td>
-                    </tr>
-                  </td>
-                  <td>
-                    <tr>
-                      <td>{match["Set3 Player 1"]}</td>
-                    </tr>
-                    <tr>
-                      <td>{match["Set3 Player 2"]}</td>
-                    </tr>
-                  </td>
-                  <td>
-                    <tr>
-                      <td className="text-success">
-                        <b>{match["Player 1 Score"]}</b>
+                      <td>
+                        <tr>
+                          <td>{match["Set1 Player 1"]}</td>
+                        </tr>
+                        <tr>
+                          <td>{match["Set1 Player 2"]}</td>
+                        </tr>
+                      </td>
+                      <td>
+                        <tr>
+                          <td>{match["Set2 Player 1"]}</td>
+                        </tr>
+                        <tr>
+                          <td>{match["Set2 Player 2"]}</td>
+                        </tr>
+                      </td>
+                      <td>
+                        <tr>
+                          <td>{match["Set3 Player 1"]}</td>
+                        </tr>
+                        <tr>
+                          <td>{match["Set3 Player 2"]}</td>
+                        </tr>
+                      </td>
+                      <td>
+                        <tr>
+                          <td className="text-success">
+                            <b>{match["Player 1 Score"]}</b>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td className="text-success">
+                            <b>{match["Player 2 Score"]}</b>
+                          </td>
+                        </tr>
                       </td>
                     </tr>
-                    <tr>
-                      <td className="text-success">
-                        <b>{match["Player 2 Score"]}</b>
-                      </td>
-                    </tr>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+                  ) : null
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
-      );
+      </div>
+    </div>
+  </div>
   
-    // return (
-    //   <div>
-    //     {/* ... Votre contenu JSX */}
-    //   </div>
-    // );
-  };
-  
-
-
-
-
-/**test*/
-
-
-// const Countries = () => {
-//     const apikeys = [ "7a3522b864msh458ad29cbfda53dp191cd6jsn8a9ecb8f7113",  "50736dc75amsh7f9e578a03cb22ap1ea0e4jsn338832cda913",  "102585ac4fmsh2631463ec6a8969p1ccd25jsn4b73c4307617"];
-//     const apikey_first = "7a3522b864msh458ad29cbfda53dp191cd6jsn8a9ecb8f7113";
-//     const apikey_second = "50736dc75amsh7f9e578a03cb22ap1ea0e4jsn338832cda913";
-//     const apikey_third = "102585ac4fmsh2631463ec6a8969p1ccd25jsn4b73c4307617";
-//     const tennis_live = "https://sports-live-scores.p.rapidapi.com/tennis/live";
-//     const tennis_bis = "https://allsportsapi2.p.rapidapi.com/api/tennis/events/18/7/2023";
-//     // const delay = 1500;
-
-//     const [donnees, setData] = useState([]);
-//     // const [list_matchs, setListMatchs] = useState([]);
-//     const [limitRequestLeft, setLimitRequestLeft] = useState(0); // Utilisation de useState pour gérer limitRequestLeft
-//     const [intervalId, setIntervalId]= useState(1000);
-//     const [apiKey, setApiKey] = useState(apikey_first); // Initialisez avec la clé d'API par défaut
-//     const [danger, setDanger] = useState(null); 
-//     const [delay, setDelay] = useState(1500); 
-
-//   useEffect(() => {
-//     const fetchData = () => {
-//       axios.get(tennis_live, {
-//         headers: { "X-RapidAPI-Key": apiKey}, // Utilisez la clé d'API actuelle
-//       })
-//       .then((res) => {
-//         // Mise à jour de la variable limit_request_left avec le nombre d'appels restants
-//         console.log(limitRequestLeft);
-
-//         // Mise à jour de l'état 'donnees' avec la réponse de l'API
-//         setData(res.data.matches);
-//         console.log(donnees);
-//         setLimitRequestLeft(res.headers['x-ratelimit-requests-remaining']);
-//       })
-//       .catch((error) => {
-//         console.log(error.config.headers['X-RapidAPI-Key']);
-//         if (error.response.headers['x-ratelimit-requests-remaining'] <= 0) {
-//           // Vous avez atteint la limite des requêtes, alors mettez à jour la clé d'API
-//           // Utilisez un index pour obtenir la prochaine clé d'API du tableau apikeys
-//           const currentIndex = apikeys.indexOf(apiKey);
-//           const nextIndex = (currentIndex + 1) % apikeys.length;
-//           const newApiKey = apikeys[nextIndex];
-//           console.log(newApiKey)
-//           setApiKey(newApiKey);
-     
-//           // Vous pouvez également stocker la nouvelle clé d'API dans le localStorage ou ailleurs
-//           setDanger(error.response.data.message + '<br> code HTTP: ' + error.request.status);
-//           // alert('Fournissez une nouvelle clé d\'API');
-//         }
-//      });
-     
-//     };
-
-//     const pause = a => { a = 1; return a++; }
-//     //const testInter = console.log(pause);
-//     // Appelez fetchData immédiatement lors du montage du composant
-//     //fetchData();
-
-//     // Utilisez setInterval pour appeler fetchData chaque minute (60000 millisecondes)
-//     const intervalId = setInterval(fetchData, delay);
-
-//     // Retournez une fonction de nettoyage pour annuler l'intervalle lors du démontage du composant
-//     return () => clearInterval(intervalId);
-//   }, [apiKey]); // Incluez apiKey dans les dépendances pour que useEffect se réexécute lorsque la clé d'API change
-
-
-
-//     return (
-//       <div>
-//         <Navigation />
-//         <div className="container">
-//           <div className="row">
-//             {danger ? (
-//               <div className="col-10 alert alert-danger mx-auto">{danger}</div>
-//             ) : null}
-//           </div>
-//         </div>
-//         <hr />
-//         <h3 className="text-center">
-//           Il te reste {limitRequestLeft} appels{" "}
-//           <button
-//             className="btn btn-primary"
-//             onClick={() => {
-//               clearInterval(intervalId); // D'abord, effacez l'intervalle actuel
-//               const newIntervalId = setInterval(fetchData, 20000); // Ensuite, créez un nouvel intervalle avec le délai souhaité
-//               setIntervalId(newIntervalId); // Mettez à jour l'état intervalId avec le nouvel identifiant d'intervalle
-//             }}
-//           >
-//             time
-//           </button>
-//         </h3>
-//         <table className="table table-striped">
-//           <thead>
-//             <tr>
-//               <th>&nbsp;&nbsp;&nbsp;</th>
-//               <th className="text-center">liste des matchs de ce jour</th>
-//               <th>&nbsp;&nbsp;&nbsp;</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {donnees.map((match, index) => (
-//               <tr key={index} className="border-bottom">
-//                 <td>
-//                   {match["Tournament"]} - {match["Surface"]}
-//                 </td>
-//                 <td>
-//                   <tr>
-//                     <td>{match["Home Player"]}</td>
-//                     <td>
-//                       <b>{match["Live Home Odd"]}</b>
-//                     </td>
-//                     {/* <td><b>{match["Sets Player 1"]}</b></td> */}
-//                   </tr>
-//                   <tr>
-//                     <td>{match["Away Player"]} &nbsp;</td>
-//                     <td>
-//                       <b>{match["Live Away Odd"]}</b>
-//                     </td>
-//                     {/* <td><b>{match["Sets Player 2"]}</b></td> */}
-//                   </tr>
-//                 </td>
-//                 <td>
-//                   <tr>
-//                     <td>{match["Sets Player 1"]}</td>
-//                   </tr>
-//                   <tr>
-//                     <td>{match["Sets Player 2"]}</td>
-//                   </tr>
-//                 </td>
-//                 <td>
-//                   <tr>
-//                     <td>{match["Set1 Player 1"]}</td>
-//                   </tr>
-//                   <tr>
-//                     <td>{match["Set1 Player 2"]}</td>
-//                   </tr>
-//                 </td>
-//                 <td>
-//                   <tr>
-//                     <td>{match["Set2 Player 1"]}</td>
-//                   </tr>
-//                   <tr>
-//                     <td>{match["Set2 Player 2"]}</td>
-//                   </tr>
-//                 </td>
-//                 <td>
-//                   <tr>
-//                     <td>{match["Set3 Player 1"]}</td>
-//                   </tr>
-//                   <tr>
-//                     <td>{match["Set3 Player 2"]}</td>
-//                   </tr>
-//                 </td>
-//                 <td>
-//                   <tr>
-//                     <td className="text-success">
-//                       <b>{match["Player 1 Score"]}</b>
-//                     </td>
-//                   </tr>
-//                   <tr>
-//                     <td className="text-success">
-//                       <b>{match["Player 2 Score"]}</b>
-//                     </td>
-//                   </tr>
-//                 </td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//       </div>
-//     );
-// };
-
-
+  );
+};
 
 export default Countries;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,22 +4,26 @@ import "./navigation.css";
 
 const Navigation = () => {
   return (
-    <div className="text-center mt-4">
-      <h1 className="text-primary text-center">API FOOT</h1>
-      <ul className="list-inline">
-        <NavLink to="/">
-          <li>home</li>
-        </NavLink>
-        <NavLink to="/about">
-          <li>about</li>
-        </NavLink>
-        <NavLink to="/pays">
-          <li>matchs</li>
-        </NavLink>
-        <NavLink to="/test">
-          <li>tester</li>
-        </NavLink>
-      </ul>
+    <div className="container">
+      <div className="row">
+        <div className="col-10 mx-auto mt-4">
+          <h1 className="text-primary text-center">API FOOT (tennis live)</h1>
+          <ul className="list-inline text-center">
+            <NavLink to="/">
+              <li>home</li>
+            </NavLink>
+            <NavLink to="/about">
+              <li>about</li>
+            </NavLink>
+            <NavLink to="/pays">
+              <li>matchs</li>
+            </NavLink>
+            <NavLink to="/test">
+              <li>tester</li>
+            </NavLink>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
