316d5c2b546647d9bb3948d9df8cf75e08aa9fbb
heinzwalace <heinzwalace@gmail.com>
2025-02-21 10:47:45 +0100

    error-zone

---
+++ src/components/ErrorZone.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const ErrorZone = () => {
+    return (
+        <div>
+          <h3 className="text-center">une erreur est survenue</h3>
+        </div>
+)}
+
+
+export default ErrorZone;
